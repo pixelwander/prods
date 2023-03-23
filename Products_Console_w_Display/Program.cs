29a472e35cef355eda592b80da04682d6c8fc9c3
dasuday <dasuday@iit.edu>
2023-03-23 10:44:08 -0700

    first commit

---
+++ Products_Console_w_Display/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+using System.Data;
+using Npgsql;
+
+class Sample
+{
+    static void Main(string[] args)
+    {
+        // Connect to a PostgreSQL database
+        NpgsqlConnection conn = new NpgsqlConnection("Server=127.0.0.1:5433;User Id=postgres; " +
+           "Password=wizardry;Database=prods;");
+        conn.Open();
+
+        // Define a query returning a single row result set
+        NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM product", conn);
+
+        // Execute the query and obtain the value of the first column of the first row
+        //Int64 count = (Int64)command.ExecuteScalar();
+        NpgsqlDataReader reader = command.ExecuteReader();
+
+        DataTable dt = new DataTable();
+        dt.Load(reader);
+
+        print_results(dt);
+
+        conn.Close();
+    }
+
+    static void print_results(DataTable data)
+    {
+        Console.WriteLine();
+        Dictionary<string, int> colWidths = new Dictionary<string, int>();
+
+        foreach (DataColumn col in data.Columns)
+        {
+            Console.Write(col.ColumnName);
+            var maxLabelSize = data.Rows.OfType<DataRow>()
+                    .Select(m => (m.Field<object>(col.ColumnName)?.ToString() ?? "").Length)
+                    .OrderByDescending(m => m).FirstOrDefault();
+
+            colWidths.Add(col.ColumnName, maxLabelSize);
+            for (int i = 0; i < maxLabelSize - col.ColumnName.Length + 14; i++) Console.Write(" ");
+        }
+
+        Console.WriteLine();
+
+        foreach (DataRow dataRow in data.Rows)
+        {
+            for (int j = 0; j < dataRow.ItemArray.Length; j++)
+            {
+                Console.Write(dataRow.ItemArray[j]);
+                for (int i = 0; i < colWidths[data.Columns[j].ColumnName] - dataRow.ItemArray[j].ToString().Length + 14; i++) Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
